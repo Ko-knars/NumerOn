549d8cf7a371d2a1f8cd33600cc23902c9c3e94a
ko-knars <knars18727@gmail.com>
2023-07-07 22:11:57 +0900

    ヌメロンの判定の修正

--- numeron.py
+++ numeron.py
@@ -8,13 +8,14 @@ def game(player,prediction):
 
     for i in range(digit):
         prediction_num = prediction[i]
+        print(prediction_num)
         if player[i] == prediction_num:
             eats = eats + 1
         
-        if any(prediction_num in player[j] for j in range(digit)):
+        if any(prediction_num  in player[j] for j in range(digit)):
             bites = bites + 1
 
-    return str(eats) + "EAT" + str(bites) + "BITE"
+    return str(eats) + "EAT" + str(bites-eats) + "BITE"
 
 player = 1
 
@@ -22,20 +23,24 @@ while True:
     
     if player % 2 == 1:
         input_prediction = list(input("予測値を入力してください（player1）"))
-        ans = game(player1_num_list, input_prediction)
+        ans = game(player2_num_list, input_prediction)
+        print(ans)
         if ans == "3EAT0BITE":
             print("player1の勝利です")
             break
         
-    if player % 2 == 2:
+    if player % 2 == 0:
         input_prediction = list(input("予測値を入れてください（player2）"))
-        ans = game(player2_num_list, input_prediction)
+        ans = game(player1_num_list, input_prediction)
+        print(ans)
         if ans == "3EAT0BITE":
             print("player2の勝利です")
             break
     player = player + 1
 
-        
+
+
+  
 
     
 
